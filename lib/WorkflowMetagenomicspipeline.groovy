2d8a791998940cb9e03176a8bc4109d9be4c5ca6
Barbara Verhaar <bjh.verhaar@gmail.com>
2025-07-27 15:20:48 +0200

    update citations, validate genome,fasta

--- lib/WorkflowMetagenomicspipeline.groovy
+++ lib/WorkflowMetagenomicspipeline.groovy
@@ -1,5 +1,5 @@
 //
-// This file holds several functions specific to the workflow/metagenomicspipeline.nf in the nf-core/metagenomicspipeline pipeline
+// This file holds several functions specific to the workflow/metagenomicspipeline.nf
 //
 
 import nextflow.Nextflow
@@ -14,10 +14,9 @@ class WorkflowMetagenomicspipeline {
 
         genomeExistsError(params, log)
 
-
-        // if (!params.fasta) {
-        //     Nextflow.error "Genome fasta file not specified with e.g. '--fasta genome.fa' or via a detectable config file."
-        // }
+        if (!params.fasta & !params.genome & !params.skip_hostfilter) {
+            Nextflow.error "Genome fasta file not specified with e.g. '--fasta genome.fa' or '--genome GRCh38'. If you don't want to filter host reads, use --skip_hostfilter true."
+        }
     }
 
     //
@@ -53,28 +52,32 @@ class WorkflowMetagenomicspipeline {
 
     public static String toolCitationText(params) {
 
-        // TODO nf-core: Optionally add in-text citation tools to this list.
-        // Can use ternary operators to dynamically construct based conditions, e.g. params["run_xyz"] ? "Tool (Foo et al. 2023)" : "",
-        // Uncomment function in methodsDescriptionText to render in MultiQC report
         def citation_text = [
                 "Tools used in the workflow included:",
                 "FastQC (Andrews 2010),",
+                !params.skip_qualityfilter & !params.skip_preprocessing ?"Fastp (Chen et al. 2018)," : "",
+                !params.skip_hostfilter & !params.skip_preprocessing ? "Bowtie2 (Langmead & Salzberg 2012)," : "",
+                !params.skip_metaphlan ? "MetaPhlAn (Beghini et al. 2021)," : "",
+                !params.skip_humann ? "HUMAnN (Franzosa et al. 2018)," : "",
+                !params.skip_kraken ? "Kraken2 (Wood et al. 2019)," : "",
+                !params.skip_kraken ? "Bracken (Lu et al. 2017)," : "",
                 "MultiQC (Ewels et al. 2016)",
-                "."
-            ].join(' ').trim()
+            ].findAll { it != "" }.join(' ').trim()
 
         return citation_text
     }
 
     public static String toolBibliographyText(params) {
-
-        // TODO Optionally add bibliographic entries to this list.
-        // Can use ternary operators to dynamically construct based conditions, e.g. params["run_xyz"] ? "<li>Author (2023) Pub name, Journal, DOI</li>" : "",
-        // Uncomment function in methodsDescriptionText to render in MultiQC report
         def reference_text = [
                 "<li>Andrews S, (2010) FastQC, URL: https://www.bioinformatics.babraham.ac.uk/projects/fastqc/).</li>",
+                (!params.skip_qualityfilter & !params.skip_preprocessing) ? "<li>Chen, S., Zhou, Y., Chen, Y., & Gu, J. (2018). fastp: an ultra-fast all-in-one FASTQ preprocessor. Bioinformatics, 34(17), i884-i890. doi: 10.1093/bioinformatics/bty560</li>" : "",
+                (!params.skip_hostfilter & !params.skip_preprocessing) ? "<li>Langmead, B., & Salzberg, S. L. (2012). Fast gapped-read alignment with Bowtie 2. Nature methods, 9(4), 357-359. doi: 10.1038/nmeth.1923</li>" : "",
+                !params.skip_metaphlan ? "<li>Beghini, F., McIver, L. J., Blanco-Míguez, A., Dubois, L., Asnicar, F., Maharjan, S., ... & Segata, N. (2021). Integrating taxonomic, functional, and strain-level profiling of diverse microbial communities with bioBakery 3. Elife, 10, e65088. doi: 10.7554/eLife.65088</li>" : "",
+                !params.skip_humann ? "<li>Franzosa, E. A., McIver, L. J., Rahnavard, G., Thompson, L. R., Schirmer, M., Weingart, G., ... & Huttenhower, C. (2018). Species-level functional profiling of metagenomes and metatranscriptomes. Nature methods, 15(11), 962-968. doi: 10.1038/s41592-018-0176-y</li>" : "",
+                !params.skip_kraken ? "<li>Wood, D. E., Lu, J., & Langmead, B. (2019). Improved metagenomic analysis with Kraken 2. Genome biology, 20(1), 1-13. doi: 10.1186/s13059-019-1891-0</li>" : "",
+                !params.skip_kraken ? "<li>Lu, J., Breitwieser, F. P., Thielen, P., & Salzberg, S. L. (2017). Bracken: estimating species abundance in metagenomics data. PeerJ Computer Science, 3, e104. doi: 10.7717/peerj-cs.104</li>" : "",
                 "<li>Ewels, P., Magnusson, M., Lundin, S., & Käller, M. (2016). MultiQC: summarize analysis results for multiple tools and samples in a single report. Bioinformatics , 32(19), 3047–3048. doi: /10.1093/bioinformatics/btw354</li>"
-            ].join(' ').trim()
+            ].findAll { it != "" }.join(' ').trim()
 
         return reference_text
     }
@@ -90,13 +93,8 @@ class WorkflowMetagenomicspipeline {
         meta["nodoi_text"] = meta.manifest_map.doi ? "": "<li>If available, make sure to update the text to include the Zenodo DOI of version of the pipeline used. </li>"
 
         // Tool references
-        meta["tool_citations"] = ""
-        meta["tool_bibliography"] = ""
-
-        // TODO Only uncomment below if logic in toolCitationText/toolBibliographyText has been filled!
-        //meta["tool_citations"] = toolCitationText(params).replaceAll(", \\.", ".").replaceAll("\\. \\.", ".").replaceAll(", \\.", ".")
-        //meta["tool_bibliography"] = toolBibliographyText(params)
-
+        meta["tool_citations"] = toolCitationText(params).replaceAll(", \\.", ".").replaceAll("\\. \\.", ".").replaceAll(", \\.", ".")
+        meta["tool_bibliography"] = toolBibliographyText(params)
 
         def methods_text = mqc_methods_yaml.text
 
